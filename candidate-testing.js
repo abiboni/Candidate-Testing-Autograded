10485d402f300c8a08a7caa0e8cf6ea66d0ff50f
abiboni <abigail.o.bonilla@gmail.com>
2023-10-31 14:30:03 -0400

    Candidate Name

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -18,13 +18,18 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("What is your name? ");
+console.log(candidateName);
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+let askQuestion;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer;
+console.log(question)
 }
 
 function gradeQuiz(candidateAnswers) {

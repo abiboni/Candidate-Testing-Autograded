1f62368404b7ec6d3877b726aedd2dc37614c005
abiboni <abigail.o.bonilla@gmail.com>
2023-11-03 12:28:58 -0400

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ",
-"True or false: 5 kilometer == 5000 meters?", 
-"(5 + 3/2 * 10 = ?",
-"Given the array [8, 'Orbit' 'Trajectory', 45], what entry is at index 2]",
-"What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+"True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -30,19 +30,20 @@ function askQuestion() {
 for (let i = 0; i < questions.length; i++) {
 candidateAnswer = input.question(questions[i]);
 candidateAnswers.push(candidateAnswer);
+console.log(`Your answer: ${candidateAnswers[i]} \nCorrect answer: ${correctAnswers[i]}\n`);
 }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-if (candidateAnswer === correctAnswer) {
-    console.log("Yippee! You landed the right answer!");
+for (let i = 0; i < candidateAnswers.length; i++) {
+if (candidateAnswers[i] === correctAnswers[i]) {
+    console.log('\nYay! That is correct!');
 } else {
-    console.log("Houston, we have a problem!");
+    console.log(`\n${candidateAnswers[i]} is incorrect. \nThe answer is ${correctAnswers[i]}!`);
   }
-
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -55,11 +56,11 @@ function runProgram() {
   // TODO 1.1c: Greet candidate using their name //
   console.log("Hello, "+ candidateName + ". Welcome to LaunchCode."); 
   askQuestion();
-  console.log("questions: ", questions);
-  console.log();
-  console.log("correct answers", correctAnswers);
-  console.log();
-  console.log("candidate answers", candidateAnswers);
+  // console.log("questions: ", questions );
+  // console.log();
+  // console.log("correct answers", correctAnswers);
+  // console.log();
+  // console.log("candidate answers", candidateAnswers);
   gradeQuiz(this.candidateAnswers);
 }
 
